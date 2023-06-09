c29b75dd4a59b1bdd1ee3ca5abc87b4556b79e6d
Christopher Thompson <101826879+Christhopas@users.noreply.github.com>
2023-06-09 13:22:00 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -1,4 +1,19 @@
- -- quando um funcionario novo for adicionado, se seu departamento for = 1, seu salario base vai ser 15000 --
+-- A criação de índices pode melhorar o desempenho das consultas, mas também pode aumentar o tempo de inserção, atualização e exclusão de registros, pois os índices precisam ser atualizados -- 
+
+-- Exemplos de algunmas otimizações que podem ser usadas quem melhoram a velocidade de consultas na tabela funcionario --
+CREATE INDEX idx_numero_departamento ON funcionario (numero_departamento);
+CREATE INDEX idx_cpf_supervisor ON funcionario (cpf_supervisor);
+CREATE INDEX idx_salario ON funcionario (salario);
+
+-- Exemplos de algunmas otimizações que podem ser usadas quem melhoram a velocidade de consultas na tabela Projeto --
+CREATE INDEX idx_numero_departamento ON projeto (numero_departamento);
+CREATE INDEX idx_nome_projeto ON projeto (nome_projeto);
+CREATE INDEX idx_local_projeto ON projeto (local_projeto);
+
+
+
+
+-- quando um funcionario novo for adicionado, se seu departamento for = 1, seu salario base vai ser 15000 --
 DELIMITER //
 CREATE TRIGGER definir_salario
 BEFORE INSERT ON funcionario
@@ -11,4 +26,4 @@ END //
 DELIMITER ;
 
 INSERT INTO funcionario(primeiro_nome, ultimo_nome, cpf, data_nascimento, endereco, sexo, salario, cpf_supervisor, numero_departamento )  
-Values ('alffredo', 'gisep', '48411203422', '1980-10-09', 'Rua do alem,373,Vila Velha, ES','M','00000', '88866555576', 1);
+Values ('ze', 'ninguem', '48411203422', '1980-10-09', 'Rua do alem,373,Vila Velha, ES','M','00000', '88866555576', 1);
